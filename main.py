d6fb4ff78096cae760fafff3813dce8c356ffbaa
Ahmed Abdel Moneim Abdel Halim ALSAIDI <125546540+ahmedabdelmoneim22@users.noreply.github.com>
2024-02-15 19:10:22 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+print("Ahmed america Python")
+print("Capstone Project - Turtle Crossing Game.")
+#################################################
+#print("Constructor")
+#class Car:
+#    def __init__(self):
+#        # initialise attributes.
+#################################
+#print("Inheritance")
+#class Fish(Animal):
+#    def __init__(self):
+#        super().__init__()
+##################################
+#print("python turtle coordinate system")
+##################################
+#Step 1 - Check out how the game play works.
+#1. A turtle moves forwards when you press the "Up" key. It can only move forwards, not back, left or right.
+
+#2. Cars are randomly generated along the y-axis and will move from the right edge of the screen to the left edge.
+#3. When the turtle hits the top edge of the screen, it moves back to the original position and the player levels up.
+#On the next level, the car speed increases.
+
+#4. When the turtle collides with a car, it's game over and everything stops.
+###################################
+#Step 2 - Break down the Problem.
+###################################
+#1)Move the turtle with Keypress.
+#2)Create and move the cars.
+#3)Detect collision with car.
+#4)Detect when turtle reaches the other side.
+#5)Create a scoreboard.
+###################################
+
